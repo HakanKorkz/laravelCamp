f3d0324b4935480f388dd0d1a2cc857367edcdd2
hakan <hakan.hizmet.54.gs@gmail.com>
2022-12-08 19:51:59 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,24 +12,3 @@ use App\Http\Controllers\UserController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::resource("users",UserController::class);
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-//
-//// Rota gurubu oluşturma işlemi
-//Route::prefix("urunler")->group(function () {
-//    Route::get("/bilgisayar", function () {
-//        return "Canavar sistem";
-//    });
-//    Route::get("/telefon", function () {
-//        return "Telefon bu";
-//    });
-//});
-//
-//// Varsayılan hata sayfası nyerine özeleştirilmiş hata sayfası oluşturabilir ve kontrol yapısı ve ara yazılım kullanabiliriz...
-//Route::fallback(function (){
-//    return "Aradığın sayfa yok birader";
-//});
