7c4dbf07be2b2a145af27f2a289d24c7ee49f3ac
hakan <hakan.hizmet.54.gs@gmail.com>
2022-12-08 18:49:20 +0300

    Route işlemleri yapıldı

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource("users",UserController::class);
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//// Rota gurubu oluşturma işlemi
+//Route::prefix("urunler")->group(function () {
+//    Route::get("/bilgisayar", function () {
+//        return "Canavar sistem";
+//    });
+//    Route::get("/telefon", function () {
+//        return "Telefon bu";
+//    });
+//});
+//
+//// Varsayılan hata sayfası nyerine özeleştirilmiş hata sayfası oluşturabilir ve kontrol yapısı ve ara yazılım kullanabiliriz...
+//Route::fallback(function (){
+//    return "Aradığın sayfa yok birader";
+//});
