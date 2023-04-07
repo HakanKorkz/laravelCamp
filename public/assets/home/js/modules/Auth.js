daf5f56792efada7a5c66623fd2dad9f3be5fdeb
hakan <hakan.hizmet.54.gs@gmail.com>
2023-04-07 23:54:53 +0300

    fixed

--- public/assets/home/js/modules/Auth.js
+++ public/assets/home/js/modules/Auth.js
@@ -15,21 +15,15 @@ export const register = () => {
         const data = new FormData(form)
         requestPost("register", data).then(response => {
             let message = ""
+            let tittle
             if (response.errors) {
-                [response.errors].forEach(error => {
-                    console.log(error)
-                    error.email.forEach(e => {
-                        message += `${e} \n`
-                    })
-                    error.name.forEach(n => {
-                        message += `${n} \n`
-                    })
-                    error.password.forEach(p => {
-                        message += `${p} \n`
-                    })
-                })
+               message=errorMessage([response.errors])
+                tittle = "İşlem başarısız"
+            } else {
+                tittle = "İşlem başarılı"
             }
-            notifications("notiflix", "error", message, "notify")
+
+            notifications("notiflix", "error", message, "report", tittle)
             Loading(false)
         })
     })
@@ -38,12 +32,31 @@ export const register = () => {
 const background = () => {
     document.addEventListener("DOMContentLoaded", function() {
         let elements = document.querySelectorAll("[data-background-url]");
-        elements.forEach(function(element) {
+        elements.forEach(function (element) {
             let url = element.getAttribute("data-background-url");
             element.style.backgroundImage = "url(" + url + ")";
             element.classList.remove("hidden")
 
         });
     });
+
+}
+
+const errorMessage = (errors) => {
+let message=""
+    errors.forEach(error => {
+        console.log(error)
+        error.email.forEach(e => {
+            message += `${e}`
+        })
+        error.name.forEach(n => {
+            message += `${n} <br>`
+        })
+        error.password.forEach(p => {
+            message += `${p} <br>`
+        })
+    })
+
+    return message
 
 }
